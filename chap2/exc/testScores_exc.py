e209ee85e46da85abbb9daf6955558d5ff0652b4
lh77x7 <lh77x7@gmail.com>
2022-07-20 20:50:43 +0200

    testScores exercise

--- chap2/exc/testScores_exc.py
+++ chap2/exc/testScores_exc.py
@@ -7,17 +7,20 @@ print("Enter 3 test scores")
 print("=======================")
 
 # get scores from the user and accumulate the total
-total_score = 0 # initialize the variable
-total_score += int(input("Enter test score: "))
-total_score += int(input("Enter test score: "))
-total_score += int(input("Enter test score: "))
+total_score1 = 0; total_score2 = 0; total_score3 = 0 # initialize the variable
+total_score1 += int(input("Enter test score: "))
+total_score2 += int(input("Enter test score: "))
+total_score3 += int(input("Enter test score: "))
+total_score = total_score1 + total_score2 + total_score3
 
 # calculate average score
-average_score = round(total_score / 3)
+average_score = round((total_score / 3), 2)
+sum = 0
 
 # format and display the result
 print("=======================")
-print("Total Score: ", total_score,
-"\nAverage Score: ", average_score)
+print("Your Scores: ", total_score1, total_score2, total_score3)
+print("Total Score: ", total_score)
+print("Average Score: ", average_score)
 print()
 print("Bye")
