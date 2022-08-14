5297f67a5339aec20add3acc2ed14ba579628b32
lh77x7 <lh77x7@gmail.com>
2022-08-14 15:57:25 +0200

    exc 1 chap 6 upload

--- chap6/exc/test_scores.py
+++ chap6/exc/test_scores.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 # display a welcome message
-from statistics import median
-
 def welcome():
     print("The Test Scores application")
     print()
@@ -10,31 +8,64 @@ def welcome():
     print("Enter 'x' to end input")
     print("======================")
 
+# get the scores
+def get_scores():
+    scores = []
+    while True:
+        score = input("Enter test score: ")
+        if score == "x":
+            return scores
+        else:
+            score = int(score)
+            if score >= 0 and score <= 100:
+                scores.append(score)
+            else:
+                print("Test score must be from 0 to 100. " +
+                "Score discarded. Try again.")
 
-# initialize variables
-counter = 0
-score_total = 0
-test_score = 0
-list = []
 
-while True:
-    test_score = input("Enter test score (or 'x' to quit): ")
-    if test_score != "x":
-        test_score = int(test_score)
-        counter += 1
+# process scores and show results
+def process_scores(scores):
+    # calculate total
+    total = 0
+    for score in scores:
+        total += score
+    
+    # calculate averaga
+    average = round(total / len(scores))
+
+    # get min and max values
+    min_value = min(scores)
+    max_value = max(scores)
+
+    # get median
+    median_index = len(scores) // 2
+    if len(scores) % 2 == 1: # odd result
+        median_score = scores[median_index]
     else:
-        break
-    if test_score >= 0 and test_score <= 100:
-        score_total += test_score
-        counter += 1
-    else:
-        print("Test score must be from 0 through 100. Score discarded. Try again.")   
+        middle1 = scores[median_index]
+        middle2 = scores[median_index - 1]
+        median_score = (middle1 + middle2) / 2
 
+    # format and display result
+    print()
+    print("Total:           ", total)
+    print("Nr Scores:       ", len(scores))
+    print("Average Score:   ", average)
+    print("Min Score:       ", min_value)
+    print("Max Score:       ", max_value)
+    print("Media Score:     ", median_score)
+          
+# main function            
 def main():
 
     welcome()
-    # dalej
+    scores = get_scores()
+    process_scores(scores)
+    print("")
+    print("Koniec!")
 
+# if started as the main module, call the main function
 if __name__ == "__main__":
     main()
 
