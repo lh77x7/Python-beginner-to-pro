07806951a1075fdaa456add6303aa9988feea7e6
lh77x7 <lh77x7@gmail.com>
2022-09-03 12:11:22 +0200

    time program

---
+++ chap11/time.py
@@ -0,0 +1,37 @@
+from datetime import datetime, time
+
+def main():
+    print("The Timer program")
+    print()
+
+    # start program
+    input("Press Enter to start...")
+    start_time = datetime.now()
+    print("Start time: ", start_time)
+    print()
+
+    # stop time
+    input("Press Enter to stop...")
+    stop_time = datetime.now()
+    print("Stop time: ", stop_time)
+    print()
+
+    # calculate elapsed time
+    elapsed_time = stop_time - start_time
+    days = elapsed_time.days
+    hours = days // 24
+    minutes = elapsed_time.seconds // 60
+    seconds = elapsed_time.seconds % 60
+    microseconds = elapsed_time.microseconds
+
+    # calculate time object
+    time_object = time(hours, minutes, seconds, microseconds)
+
+    # display results
+    print("ELAPSED TIME")
+    if days > 0:
+        print("Days: ", days)
+    print("Time: ", time_object)
+
+if __name__ == "__main__":
+    main()
