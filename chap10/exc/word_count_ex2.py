33fa951ff23f848440dccc9ac346ccf33dc4791e
lh77x7 <lh77x7@gmail.com>
2022-10-01 16:10:42 +0200

    ex2 chap 10

---
+++ chap10/exc/word_count_ex2.py
@@ -0,0 +1,47 @@
+from os.path import dirname, join
+current_dir = dirname(__file__)
+FILENAME = "movies.txt" # operate on txt file
+file_path = join(current_dir, FILENAME)
+
+def get_words_from_file(file_path):
+    with open(file_path,  errors="ignore") as file:
+        text = file.read() # read str from file
+
+    text = text.replace("\n", "")
+    text = text.replace(",", "")
+    text = text.replace(".", "")
+    text = text.lower()
+
+    words = text.split(" ") # convert str to list
+    words.sort()
+    return words
+
+def get_unique_words(words):
+    unique_words = []
+    unique_words.append(words[0])
+
+    for i in range(1, len(words)):
+        if words[i] == words[i - 1]:
+            continue
+        else:
+            unique_words.append(words[i])
+    return unique_words
+
+def main():
+    print("The Word Counter program\n")
+
+    # get words and unique words
+    words = get_words_from_file(file_path) # get list of words
+    unique_words = get_unique_words(words)
+
+    # display number of words and unique words
+    print("Number of words = ", len(words))
+    print("Number of unique words = ", len(unique_words))
+
+    # display unique words and their word counts
+    print("Unique word occurrences:")
+    for word in unique_words:
+        print("     ", word, "-", words.count(word))
+
+if __name__ == "__main__":
+    main()
